a772e6ce30bf331df23786f5111a69e657abc41d
Andreas Kähäri <andreas.kahari@nbis.se>
2025-09-08 15:44:11 +0200

    fix: Simplify
    Among other things: Avoid having the Compose file hand environment
    variables to the container.

--- app/start-script.sh
+++ app/start-script.sh
@@ -2,21 +2,15 @@
 
 set -u
 
-export XDG_CONFIG_HOME="$APP_VOLUME_PATH/caddy/config"
-export XDG_DATA_HOME="$APP_VOLUME_PATH/caddy/data"
+export XDG_CONFIG_HOME="$MY_APP_VOLUME_PATH/caddy/config"
+export XDG_DATA_HOME="$MY_APP_VOLUME_PATH/caddy/data"
 
 install -d "$XDG_CONFIG_HOME"
 install -d "$XDG_DATA_HOME"
 
-export SITE_ROOT=$APP_VOLUME_PATH/www
+export SITE_ROOT=$MY_APP_VOLUME_PATH/www
 
-while true; do
-	printf 'Starting Caddy to serve static content from "%s"\n' \
-		"$SITE_ROOT" >&2
+printf 'Starting Caddy to serve static content from "%s"\n' \
+	"$SITE_ROOT" >&2
 
-	caddy run --config="$HOME/Caddyfile"
-
-	printf 'Caddy exited with status=%d\n' "$?" >&2
-	echo 'Sleeping 30s before restarting...' >&2
-	sleep 30
-done
+exec caddy run --config="$HOME/Caddyfile"
